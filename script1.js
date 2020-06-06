c46ca622cdf73d9e8e9c4b7ef7c8cb790c81d831
pantekzzzz <64633690+pantekzzzz@users.noreply.github.com>
2020-06-06 21:52:20 +0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -407,7 +407,7 @@
         // shuffle the strings
         shuffle: false,
         // time before backspacing
-        backDelay: 500,
+        backDelay: 5000,
         // loop
         loop: false,
         // false = infinite
